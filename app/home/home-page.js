5cb5972234bb3fb4eb6ab8779f0e66530c2f59f1
Todor Totev <totev@progress.com>
2018-05-22 17:05:51 +0300

    Optimal Source Code

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -6,15 +6,48 @@ logic, and to set up your page’s data binding.
 
 const HomeViewModel = require("./home-view-model");
 const Toast = require("nativescript-toast");
+const Kinvey = require('kinvey-nativescript-sdk').Kinvey;
+Kinvey.init({
+    appKey: 'X',
+    appSecret: 'X'
+});
 
-function onNavigatingTo(args) {
+exports.onNavigatingTo = function onNavigatingTo(args) {
     const page = args.object;
     page.bindingContext = new HomeViewModel();
+};
+
+const activeUser = Kinvey.User.getActiveUser();
+const userPromise = activeUser ? activeUser.me() : Kinvey.User.login("todor", "pass"); // DANGER! NOT PRODUCTION CODE!
+let isLogged = false;
+
+function showToast(msg, param) {
+    console.log(msg);
+    Toast.makeText(msg, param).show();
 }
 
-exports.onNavigatingTo = onNavigatingTo;
+userPromise
+    .catch(userError => {
+        if (userError.name === 'InvalidCredentialsError') {
+            // clear the activeUser using logout and then initiate login and return login response
+            return Kinvey.User.logout()
+                .then((logoutResult) => Kinvey.User.login(user.username, user.password));
+        }
+        // if the error is different from InvalidCredentialsError, forward it down the chain
+        return Promise.reject(userError);
+    })
+    .then(user => {
+        isLogged = true;
+        showToast("User is logged in");
+    })
+    .catch(e => showToast(e.message, "long"));
+
 exports.onScanResult = (scanResult) => {
-    const msg = `onScanResult6: ${scanResult.text} (${scanResult.format})`;
-    console.log(msg);
-    const toast = Toast.makeText(msg, "long").show();
+    const msg = isLogged ? `${scanResult.text} (${scanResult.format})` : "User log in is in progress, please wait";
+    const toast = showToast(msg);
+    
+    const barCodesDataStore = Kinvey.DataStore.collection("barCodesScans");
+    barCodesDataStore.save({ code: scanResult.text, format: scanResult.format })
+        .then(data => showToast("Barcode was saved"))
+        .catch(err => showToast(err.message, "long"));
 }
